52628bb2cff797f03a086b78b41f96ee20da784e
Hou <houhou1106@gmail.com>
2019-11-29 14:05:17 +0800

    add some message

--- hw3/AES.py
+++ hw3/AES.py
@@ -1,25 +1,39 @@
+# B10615046
+
 import numpy as np
+import os
 import sys
 from PIL import Image
 from Crypto.Cipher import AES
 
-ppmPic = './myppm.ppm'
+# 讀取指令
 pic = sys.argv[1]
 method = sys.argv[2]
 func = sys.argv[3]
-# image_input = Image.open('./penguin.png')
 image_input = Image.open('./'+ pic)
 
+# 檢查 Byte 
+# ppmPic = './myppm.ppm'
+# img_ppm = image_input.convert('RGB')
+# img_ppm.save(ppmPic)
+
+# 將圖片的Bytes取出作處理, mode為 RGB
 img_byte = image_input.convert('RGB').tobytes()
 size = image_input.size
 
+# print(img_byte[0:16])
+
 def AES_encrypt(text):
+    # 預設Key值 套library的function 
+    # 由於ECB CBC在block cipher的部分都是ECB 因此可共用這個 encrypt function
     key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x00'
     cipher = AES.new(key, AES.MODE_ECB)
     ciphertext = cipher.encrypt(text)
     return ciphertext
 
 def AES_decrypt(text):
+    # 預設Key值 套library的function 
+    # 由於ECB CBC在block cipher的部分都是ECB 因此可共用這個 decrypt function
     key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x00'
     cipher = AES.new(key, AES.MODE_ECB)
     plaintext = cipher.decrypt(text)
@@ -27,8 +41,11 @@ def AES_decrypt(text):
  
 def ECB_encrypt(input_byte):
     encrypt_size = len(input_byte)
+    # 開一個空的Byte 以利處理後一段一段接起來
     encrypt_byte = b''
+    # 每次取16Bytes出來作加密 最後接起來
     for i in range(0, encrypt_size, 16):
+        # 這個if else 主要是在最後一次要做padding 我只用單純的補0
         if(i + 16 <= encrypt_size):
             plaintext = input_byte[i:i+16]
         else:
@@ -39,15 +56,17 @@ def ECB_encrypt(input_byte):
             encrypt_byte = AES_encrypt(plaintext)
         else:
             encrypt_byte += AES_encrypt(plaintext)
+    # 匯出
     img = Image.frombytes("RGB",size,encrypt_byte)
-    #img.save('./penguin_encrypt.png')
     img.save('./ECB_encrypt.png')
     return encrypt_byte
 
 def ECB_decrypt(input_byte):
+    # 和加密一樣
     decrypt_byte = b''
     decrypt_size = len(input_byte)
 
+    # 除了Call的是decrypt function 其餘皆一樣
     for i in range(0, decrypt_size, 16):
         if(i + 16 <= decrypt_size):
             ciphertext = input_byte[i:i+16]
@@ -60,21 +79,25 @@ def ECB_decrypt(input_byte):
         else:
             decrypt_byte += AES_decrypt(ciphertext)
     img = Image.frombytes("RGB",size,decrypt_byte)
-    #img.save('./penguin_decrypt.png')
     img.save('./ECB_decrypt.png')
     return decrypt_byte
 
 def xor(a,b):
+    # 將byte轉成int做XOR再轉回
     int_a = int.from_bytes(a,sys.byteorder)
     int_b = int.from_bytes(b,sys.byteorder)
     int_concat = int_a ^ int_b
     return int_concat.to_bytes(len(a),sys.byteorder)
 
 def CBC_encrypt(input_byte):
+    # 基本上和ECB做的事也類似 以下會特別標出不同點
     encrypt_size = len(input_byte)
     encrypt_byte = b''
+    # 第一次加密plaintext需要與IV做XOR 後續的plaintext則跟前一個ciphertext做XOR
     IV = b'\xFF\xFE\xFD\xFC\xFB\xFA\xF9\xF8\xF7\xF6\xF5\xF4\xF3\xF2\xF1\xF0'
+    # 儲存ciphertext至下個block
     next_state = b''
+    # 處理方式同ECB 只是多了需要先xor再做block cipher
     for i in range(0, encrypt_size, 16):
         if(i + 16 <= encrypt_size):
             plaintext = input_byte[i:i+16]
@@ -90,12 +113,13 @@ def CBC_encrypt(input_byte):
             plaintext = xor(plaintext, next_state)
             next_state = AES_encrypt(plaintext)
             encrypt_byte += next_state
+    # 匯出
     img = Image.frombytes("RGB",size,encrypt_byte)
-    #img.save('./penguin_encrypt.png')
     img.save('./CBC_encrypt.png')
     return encrypt_byte
 
 def CBC_decrypt(input_byte):
+    # 與加密相同 差別只在一個先XOR 一個後XOR
     decrypt_size = len(input_byte)
     decrypt_byte = b''
     IV = b'\xFF\xFE\xFD\xFC\xFB\xFA\xF9\xF8\xF7\xF6\xF5\xF4\xF3\xF2\xF1\xF0'
@@ -107,6 +131,7 @@ def CBC_decrypt(input_byte):
             ciphertext = input_byte[i:decrypt_size]
             for j in range(16 - len(ciphertext)):
                 ciphertext += b'\x00'
+        # block cipher後才XOR
         if i == 0:
             encrypt_byte = AES_decrypt(ciphertext)
             encrypt_byte = xor(encrypt_byte, IV)
@@ -115,11 +140,12 @@ def CBC_decrypt(input_byte):
             encrypt = AES_decrypt(ciphertext)
             encrypt_byte += xor(encrypt, next_state)
             next_state = ciphertext
+    # 匯出
     img = Image.frombytes("RGB",size,encrypt_byte)
-    #img.save('./penguin_encrypt.png')
     img.save('./CBC_decrypt.png')
     return encrypt_byte
 
+# 判斷method與function
 if method == 'ECB':
     if func == 'e':
         ECB_encrypt(img_byte)
